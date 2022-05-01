06cbc16f64cc03268779ba5f2c68ec192cc064fb
Alson33 <garbuja12alson@gmail.com>
2022-05-01 10:25:36 +0545

    component: add user header component

---
+++ src/components/UserHeader.jsx
@@ -0,0 +1,14 @@
+
+const UserHeader = () => {
+  return (
+    <header className="flex">
+        <div className="some">
+            <span className="logo">LOGO</span>
+            <div className="company-name">GCES Library Management System</div>
+        </div>
+        <span className="mode">Mode</span>
+    </header>
+  )
+}
+
+export default UserHeader
