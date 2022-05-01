53152c8c4bbbf86a8a8b1575d186f72bfa6b2b47
Salipa-Gurung <salipagurung@gmail.com>
2022-05-01 15:38:37 +0545

    pages: add login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,20 @@
+import {InputField, SubmitButton} from "../../components/form/Fields"
 import UserLayout from "../../layouts/UserLayout"
+import '../../styles/form/authform.css'
 
 const Login = () => {
   return (
     <UserLayout>
       <main>
-        LogIn
+        <div className="auth-form">
+          <h2>Login</h2>
+          <form>
+            <InputField name="username" text="Username" onChange={()=>{}}/>
+            <InputField name="password" text="Password" type="password" onChange={()=>{}}/>
+            <SubmitButton text="Log in" />
+          </form>
+          <p>Forgot Password ?</p>
+        </div>
       </main>
     </UserLayout>
   )
