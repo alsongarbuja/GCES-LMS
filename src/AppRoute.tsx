c3b672d0b7bdc3c2290e98f62798cf369ac584e2
Salipa-Gurung <salipagurung@gmail.com>
2022-05-01 18:45:33 +0545

    route: add user router

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -3,12 +3,16 @@ import ForgotPassword from './pages/auth/ForgotPassword'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import ResetPassword from './pages/auth/ResetPassword'
+import BookDetail from './pages/users/BookDetail'
+import Explore from './pages/users/Explore'
+import Profile from './pages/users/Profile'
 
 const AppRoute = () => {
   return (
     <Routes>
         <Route path="/" element={<div>Welcome</div>}/>
         <Route path="/auth/*" element={<AuthRoutes/>} />
+        <Route path="/user/*" element={<UserRoutes/>} />
     </Routes>
   )
 }
@@ -24,4 +28,13 @@ const AuthRoutes = () => {
   )
 }
 
+const UserRoutes = () => {
+  return(
+    <Routes>
+        <Route path="/explore" element={<Explore/>}/>
+        <Route path="/detail/:bookId" element={<BookDetail/>} />
+        <Route path="/profile" element={<Profile/>} />
+    </Routes>
+  )
+}
 export default AppRoute
