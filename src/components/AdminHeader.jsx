c52d3709dc1969c397b406325005e55ba1cc0f35
Alson33 <garbuja12alson@gmail.com>
2022-05-01 10:24:10 +0545

    component: add admin header component

---
+++ src/components/AdminHeader.jsx
@@ -0,0 +1,17 @@
+
+const AdminHeader = () => {
+  return (
+    <header className="flex">
+        <div className="some">
+            <span className="logo">LOGO</span>
+            <div className="company-name">GCES Library Management System</div>
+        </div>
+        <div className="utils">
+            <span>Bell</span>
+            <span>Profile</span>
+        </div>
+    </header>
+  )
+}
+
+export default AdminHeader
