c09bfdaf8e508f77d0502f32bb923237cd8e713b
Alson33 <garbuja12alson@gmail.com>
2022-05-01 10:26:17 +0545

    page: add dashboard page

---
+++ src/pages/admin/Dashboard.tsx
@@ -0,0 +1,13 @@
+import AdminLayout from "../../layouts/AdminLayout"
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <main>
+          Dashboard
+      </main>
+    </AdminLayout>
+  )
+}
+
+export default Dashboard
