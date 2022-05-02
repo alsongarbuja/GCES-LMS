b282f1271ca8e136fbc20a15f84f1eb49f226b48
Alson33 <garbuja12alson@gmail.com>
2022-05-02 23:07:16 +0545

    clean-up: add icons and import styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,21 +1,36 @@
+import { FiLayout, FiBook, FiLayers, FiAlertCircle, FiUsers, FiSliders, FiLogOut, FiBookmark } from 'react-icons/fi'
+import '../styles/layout/sidebar.css'
 
 const Sidebar = () => {
   return (
-    <nav className="sidebar">
-        <ul>
-            <li>Dashboard</li>
-            <li>Borrows</li>
-            <li>Books</li>
-            <li>Categories</li>
-            <li>Limit</li>
-            <li>Students</li>
+    <nav className="flex justify-space-between sidebar">
+        <ul className="sidenav">
+            <li>
+              <FiLayout className="icons" />
+            </li>
+            <li>
+              <FiBookmark className="icons" />
+            </li>
+            <li>
+              <FiBook className="icons" />
+            </li>
+            <li>
+              <FiLayers className="icons" />
+            </li>
+            <li>
+              <FiAlertCircle className="icons" />
+            </li>
+            <li>
+              <FiUsers className="icons" />
+            </li>
         </ul>
         <ul className="settings">
-            <li>Settings</li>
             <li>
-                logout
-                <br />
-                V 1.0
+              <FiSliders className="icons" />
+            </li>
+            <li>
+              <FiLogOut className="icons" />
+              <h5>V 1.0</h5>
             </li>
         </ul>
     </nav>
