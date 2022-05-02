77ec3cc6e4f3d864a9653c1a6f6dbd15cf52a96f
Alson33 <garbuja12alson@gmail.com>
2022-05-02 23:08:02 +0545

    fix: fix admin layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,14 +1,17 @@
 import AdminHeader from '../components/AdminHeader'
 import Sidebar from '../components/Sidebar'
 import Props from '../types/props'
+import '../styles/layout/layout.css'
 
 const AdminLayout: React.FC<Props> = ({ children }) => {
   return (
     <>
         <AdminHeader />
-        <div>
+        <div className='flex flex-start'>
             <Sidebar />
-            {children}
+            <div className="main">
+              {children}
+            </div>
         </div>
     </>
   )
