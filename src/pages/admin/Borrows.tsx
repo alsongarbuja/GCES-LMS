b6b060b3aabca75451e80a41c36b43cb0ede5676
Alson33 <garbuja12alson@gmail.com>
2022-05-11 23:05:38 +0545

    dynamic: fetch dynamic borrow list

--- src/pages/admin/Borrows.tsx
+++ src/pages/admin/Borrows.tsx
@@ -1,23 +1,47 @@
+import moment from "moment"
+import { useEffect, useState } from "react"
+import { universalAPI } from "../../api/api"
 import TableLayout from "../../layouts/crud/TableLayout"
+import { BorrowModel } from "../../types/models"
 
 const Borrows = () => {
+
+    const [borrows, setBorrows] = useState<BorrowModel[]>([])
+
+    const fetchBorrows = async () => {
+        const { data, status, message } = await universalAPI('GET', '/borrow')
+        if(status==='success'){
+            setBorrows(data)
+        }else{
+            console.error(message);
+        }
+    }
+
+    useEffect(() => {
+        fetchBorrows()
+    }, [])
+
   return (
     <main>
         <h2>Borrows</h2>
         <TableLayout theads={['SN', 'Book name', 'Name', 'Due date', 'Book Id', 'Queue']} >
             <tbody>
-                <tr>
-                    <th>1</th>
-                    <td>Artificial Intelligence</td>
-                    <td>Alson Garbuja</td>
-                    <td>4 days</td>
-                    <td>AI-12345</td>
-                    <td>None</td>
-                    <td className="action-col">
-                        <button className="btn btn-success">Check In</button>
-                        <button className="btn btn-accent">Extend</button>
-                    </td>
-                </tr>
+                {
+                    borrows.map((borrow, i) => (
+                        <tr key={borrow._id}>
+                            <th>{i+1}</th>
+                            <td>{borrow.bookName}</td>
+                            <td>{borrow.userName}</td>
+                            <td>{moment(borrow.dueDate).fromNow()}</td>
+                            <td>{borrow.uniqueId}</td>
+                            <td>None</td>
+                            <td className="action-col">
+                                <button className="btn btn-success">Check In</button>
+                                <button className="btn btn-accent">Extend</button>
+                            </td>
+                        </tr>
+                    ))
+                }
             </tbody>
         </TableLayout>
     </main>
