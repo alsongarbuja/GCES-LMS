b4c6034c1488e0f435340bf98687067379ce0125
Alson33 <garbuja12alson@gmail.com>
2022-05-11 15:34:28 +0545

    dynamic: add dynamic list of book on explore

--- src/pages/users/Explore.tsx
+++ src/pages/users/Explore.tsx
@@ -2,72 +2,192 @@ import { Link } from 'react-router-dom'
 import ProfileFAB from '../../components/user/ProfileFAB'
 import '../../styles/user/style.css'
 import '../../styles/user/explore.css'
+import React, { useEffect, useState } from 'react'
+import { BookModel } from '../../types/models'
+import { universalAPI } from '../../api/api'
+import { InputField, Select } from '../../components/form/Fields'
 
 const Explore = () => {
+
+    const [books, setBooks] = useState<BookModel[]>([])
+    const [categories, setCategories] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+    const [hasSearch, setHasSearch] = useState(false)
+    const [category, setCategory] = useState('6th Semester')
+
+    const fetchBooks = async () => {
+        const { data, status, message } = await universalAPI('GET', '/books')
+
+        if(status==='success'){
+            setBooks(data)
+        }else{
+            console.error(message);
+        }
+    }
+    const fetchCategories = async () => {
+        const { data, status, message } = await universalAPI('GET', '/category')
+        if(status==='success'){
+            setCategories(data.map((c: { name: string }) => ({ value: c.name, option: c.name })))
+        }else{
+            console.error(message);
+        }
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setCategory(e.target.value)
+    const searchResult = (books: BookModel[]) => {
+        let remainingBooks = []
+        const pattern = "/" + searchTerm + "/i"
+
+        for (let i = 0; i < books.length; i++) {
+            if(
+                books[i].title.match(pattern) || books[i].secondary_title?.match(pattern)
+                || books[i].author.match(pattern) || books[i].publisher.match(pattern)
+            ){
+                remainingBooks.push(books[i])
+            }
+        }
+
+        return remainingBooks;
+    }
+    const handleSearch = (e: React.FormEvent) => {
+        e.preventDefault()
+        if(searchTerm===''){
+            setHasSearch(false)
+        }else{
+            setHasSearch(true)
+
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories()
+        fetchBooks()
+    }, [])
+
   return (
     <main>
         <ProfileFAB/>
-        <section className="box-section search-section">Search</section>
-        <section className="box-section textbook-section">
-            <div className='section-title flex justify-space-between'>
-                <h3><u>Text Books</u></h3>
-                <select name="" id="">
-                    <option value="first-sem">1st semester</option>
-                    <option value="third-sem">3rd semester</option>
-                    <option value="fifth-sem">5th semester</option>
-                </select>
-            </div>
-            <div className='book-list'>
-                <ul>
-                    <li className='flex justify-space-between single-book'>
-                        <div>
-                            <h4>Computer Networks - 2pcs available</h4>
-                            <i>Luther Author</i>
-                        </div>
-                        <span><Link to={'/user/detail/123abc'}>+</Link></span>
-                    </li>
-                    <li className='flex justify-space-between single-book'>
-                        <div>
-                            <h4>Computer Networks - 2pcs available</h4>
-                            <i>Luther Author</i>
-                        </div>
-                        <span>+</span>
-                    </li>
-                    <li className='flex justify-space-between single-book'>
-                        <div>
-                            <h4>Computer Networks - 2pcs available</h4>
-                            <i>Luther Author</i>
-                        </div>
-                        <span>+</span>
-                    </li>
-                </ul>
-                <div className='load-wrapper'><button className='btn btn-accent'>Load More</button></div>
-            </div>
+        <section className="box-section search-section">
+            <form onClick={handleSearch}>
+                <div className="row m-0">
+                    <InputField 
+                        name="searchTerm"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{setSearchTerm(e.target.value); if(e.target.value==='')setHasSearch(false)}}
+                        text=""
+                        opt="col-11 m-0"
+                        placeholder="Search..."
+                        value={searchTerm}
+                    />
+                    <button className='btn btn-accent'>Search</button>
+                </div>
+            </form>
         </section>
-        <section className="box-section referencebook-section">
-        <div className='section-title'>
-                <h3><u>Reference Books</u></h3>
-            </div>
-            <div className='book-list'>
-                <ul>
-                    <li className='flex justify-space-between single-book'>
-                        <div>
-                            <h4>Computer Networks - 2pcs available</h4>
-                            <i>Luther Author</i>
+        {
+            hasSearch?(
+                <section className="box section">
+                    <div className='section-title'>
+                        <h3><u>Search Result</u></h3>
+                    </div>
+                    <div className='book-list'>
+                        <ul>
+                            {
+                                searchResult(books)
+                                    .map(b => (
+                                        <Link to={`/user/detail/${b._id}`} key={b._id}>
+                                            <li className='single-book'>
+                                                <div className='flex justify-space-between'>
+                                                    <h4>{b.title}</h4>
+                                                    <p>{b.quantity-b.borrowed_quantity}pcs available</p>
+                                                </div>
+                                                <i>{b.author}</i>
+                                            </li>
+                                        </Link>
+                                    ))
+                            }
+                        </ul>
+                    </div>
+                </section>
+            ):(
+                <>
+                    <section className="box-section textbook-section">
+                        <div className='section-title flex justify-space-between'>
+                            <h3><u>Text Books</u></h3>
+                            <Select 
+                                name='category'
+                                options={categories}
+                                text=""
+                                value={category}
+                                onChange={handleChange}
+                            />
                         </div>
-                        <span>+</span>
-                    </li>
-                    <li className='flex justify-space-between single-book'>
-                        <div>
-                            <h4>Computer Networks - 2pcs available</h4>
-                            <i>Luther Author</i>
+                        <div className='book-list'>
+                            <ul>
+                                {
+                                    books.filter(book => (book.type==='text-book'&&book?.category===category))
+                                        .map(b => (
+                                            <Link to={`/user/detail/${b._id}`} key={b._id}>
+                                                <li className='single-book'>
+                                                    <div className="flex justify-space-between">
+                                                        <h4>{b.title}</h4>
+                                                        <p>{b.quantity-b.borrowed_quantity}pcs available</p>
+                                                    </div>
+                                                    <i>{b.author}</i>
+                                                </li>
+                                            </Link>
+                                        ))
+                                }
+                            </ul>
                         </div>
-                        <span>+</span>
-                    </li>
-                </ul>
-                <div className='load-wrapper'><button className='btn btn-accent'>Load More</button></div>
-            </div>
-        </section>
+                    </section>
+                    <section className="box-section referencebook-section">
+                        <div className='section-title'>
+                            <h3><u>Reference Books</u></h3>
+                        </div>
+                        <div className='book-list'>
+                            <ul>
+                                {
+                                    books.filter(book => (book.type==='reference'&&book?.category===category))
+                                        .map(b => (
+                                            <Link to={`/user/detail/${b._id}`} key={b._id}>
+                                                <li className='single-book'>
+                                                    <div className='flex justify-space-between'>
+                                                        <h4>{b.title}</h4>
+                                                        <p>{b.quantity-b.borrowed_quantity}pcs available</p>
+                                                    </div>
+                                                    <i>{b.author}</i>
+                                                </li>
+                                            </Link>
+                                        ))
+                                }
+                            </ul>
+                        </div>
+                    </section>
+                    <section className="box-section otherbook-section">
+                        <div className='section-title'>
+                            <h3><u>Other Books</u></h3>
+                        </div>
+                        <div className='book-list'>
+                            <ul>
+                                {
+                                    books.filter(book => (book.type==='others'))
+                                        .map(b => (
+                                            <Link to={`/user/detail/${b._id}`} key={b._id}>
+                                                <li className='single-book'>
+                                                    <div className='flex justify-space-between'>
+                                                        <h4>{b.title}</h4>
+                                                        <p>{b.quantity-b.borrowed_quantity}pcs available</p>
+                                                    </div>
+                                                    <i>{b.author}</i>
+                                                </li>
+                                            </Link>
+                                        ))
+                                }
+                            </ul>
+                        </div>  
+                    </section>
+                </>
+            )
+        }
     </main>
   )
 }
