23fa725ac561426b7421b34779fc9c759a8ef5ca
Alson33 <garbuja12alson@gmail.com>
2022-05-18 08:08:56 +0545

    redesign: use user icon for letter P for profile

--- src/components/user/ProfileFAB.tsx
+++ src/components/user/ProfileFAB.tsx
@@ -1,9 +1,12 @@
+import { FiUser } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
 const ProfileFAB = () => {
   return (
     <Link to={'/user/profile'}>
-        <span className='profile-fab box-shadow'>P</span>
+        <span className='profile-fab box-shadow'>
+          <FiUser />
+        </span>
     </Link>
   )
 }
